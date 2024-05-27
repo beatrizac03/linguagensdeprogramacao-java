ee9fde4cfd9c844935966673296ef875d0be1e16
Beatriz Aguiar <134962161+beatrizac03@users.noreply.github.com>
2024-05-27 18:39:54 -0300

    API gson

---
+++ ConversaoJavaJSON/src/ConverteJavaJSON.java
@@ -0,0 +1,26 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+public class ConverteJavaJSON {
+    public static void main(String[] args) {
+        Informacoes obj = new Informacoes();
+        Gson gson = new Gson();
+
+        // converte objetos Java para JSON e retorna JSON como String
+        String json = gson.toJson(obj);
+
+        try {
+            // Escreve Json convertido em arquivo chamado "file.json"
+            FileWriter writer = new FileWriter("file.json");
+            writer.write(json);
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(json);
+
+    }
+}
---
+++ ConversaoJavaJSON/src/Informacoes.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Informacoes {
+    private int cod_id = 500;
+    private String nome = "Bia";
+
+    private List<String> listaPreferencias = new ArrayList<String>() {
+        {
+            add("Javascript");
+            add("Java");
+            add("PHP");
+        }
+    };
+
+    public int getCod_id() {
+        return cod_id;
+    }
+
+    public void setCod_id(int cod_id) {
+        this.cod_id = cod_id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<String> getListaPreferencias() {
+        return listaPreferencias;
+    }
+
+    public void setListaPreferencias(List<String> listaPreferencias) {
+        this.listaPreferencias = listaPreferencias;
+    }
+
+    public String toString() {
+        return "Informacoes [id =" + cod_id + ", Nome =" + nome + ", Preferências =" + listaPreferencias + "]";
+    }
+
+}
